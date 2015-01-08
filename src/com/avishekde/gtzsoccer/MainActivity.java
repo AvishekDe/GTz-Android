8b212f283d434aff9e835f96bfa65b1b629cb097
AvishekDe <avishekde.iitr@gmail.com>
2015-01-09 02:08:11 +0530

    Implemented different views for the two homescreen tabs

--- src/com/avishekde/gtzsoccer/MainActivity.java
+++ src/com/avishekde/gtzsoccer/MainActivity.java
@@ -139,7 +139,7 @@ public class MainActivity extends FragmentActivity implements
 				return getString(R.string.title_section1).toUpperCase(l);
 			case 1:
 				return getString(R.string.title_section2).toUpperCase(l);
-			
+
 			}
 			return null;
 		}
@@ -162,14 +162,23 @@ public class MainActivity extends FragmentActivity implements
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main_dummy,
-					container, false);
-			TextView dummyTextView = (TextView) rootView
-					.findViewById(R.id.section_label);
-			dummyTextView.setText(Integer.toString(getArguments().getInt(
-					ARG_SECTION_NUMBER)));
+			int temp = getArguments().getInt(ARG_SECTION_NUMBER);
+			View rootView;
+			if (temp == 1) {
+				rootView = inflater.inflate(R.layout.fragment_main_dummy,
+						container, false);
+				TextView dummyTextView = (TextView) rootView
+						.findViewById(R.id.section_label_dummy);
+				dummyTextView.setText("Admin Panel");
+			} else {
+				rootView = inflater.inflate(R.layout.fragment_guest, container,
+						false);
+				TextView dummyTextView = (TextView) rootView
+						.findViewById(R.id.section_label_guest);
+				dummyTextView.setText("Guest");
+			}
+
 			return rootView;
 		}
 	}
-
 }
